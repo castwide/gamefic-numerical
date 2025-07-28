3f16e080d7fe04f74d32cf32cd8af07a9d49694e
Fred Snyder <fsnyder@castwide.com>
2025-07-27 23:20:06 -0400

    First commit

---
+++ lib/gamefic/numerical.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "numerical/version"
+
+module Gamefic
+  module Numerical
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
